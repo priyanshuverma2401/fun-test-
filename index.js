0c6a18516ade73fbe2a7b7afef5aad04a48ecd03
priyanshu <priyanshukumarv984@gmail.com>
2024-12-26 10:50:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+// const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send("genuinepriyansh")
+})
+
+app.get('/login', (req, res) => {
+  res.send("<h1>plese login to continue</h>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
